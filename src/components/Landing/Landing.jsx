21c7f5e3324350468a410e855b821692917e0bac
Kevin_Padilla <kevin0696s@gmail.com>
2023-12-14 13:40:03 -0400

    Filters almost finished

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,16 @@
 import { Box } from "@mui/system";
 import Carousel from './Carousel'
+import CardShoe from "../Card/Card";
 
 const Landing = () => {
         return (
-            <Box sx={{display: 'flex', justifyContent: 'center'}} >
+            <Box sx={{display: 'flex', flexDirection:"column", justifyContent: 'center'}} >
                 <Carousel/>
+                <CardShoe/>
             </Box>
+            
+
+            
         );
     };
 
