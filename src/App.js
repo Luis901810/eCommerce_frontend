4e8ea061ccc95fa6d356c0a5bfd8a8c0121af439
unknown <luis901810@gmail.com>
2023-12-14 14:26:07 -0500

    subo avances de login

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import Login from './components/Login/Login';
 import ShoeDetail from './components/ShoeDetail/ShoeDetail';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 import NavBar from './components/NavBar/NavBar';
+import { AuthProvider } from './components/AuthContext/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <AuthProvider>
       <Routes>
         <Route path='/'element={<Landing/>}/>
+        
         <Route path='/Catalogue' element={<Catalogue/>}/>
         <Route path='/Login' element={<Login/>}></Route> 
         <Route path='/Detail/:id'element={<ShoeDetail/>}/>
         <Route path='/ShoppingCart' element={<ShoppingCart/>} >CREATEACTIVITY</Route>
+
       </Routes>
+      </AuthProvider>
 
     </div>
   );
