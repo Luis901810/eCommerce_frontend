306402e6384b7e624ddffaa24e837da02f289b1b
Alexis Porcelano <alexisporcelano@gmail.com>
2023-12-21 12:31:26 -0300

    rango de precios y estilos

--- src/views/Catalogue/Filters.jsx
+++ src/views/Catalogue/Filters.jsx
@@ -3,6 +3,8 @@ import {
   ListSubheader,
   ListItemButton,
   ListItemText,
+  TextField,
+  Box,
 } from '@mui/material'
 import { useEffect, useState } from 'react'
 import ExpandLess from '@mui/icons-material/ExpandLess'
@@ -13,6 +15,58 @@ import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import { useDispatch, useSelector } from 'react-redux'
 import { filter } from '../../redux/actions'
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import PropTypes from 'prop-types'
+import Slider, { SliderThumb } from '@mui/material/Slider'
+
+const AirbnbSlider = styled(Slider)(({ theme }) => ({
+  color: '#42e268',
+  height: 3,
+  width: '275px',
+  marginTop: 10,
+  padding: '13px 0',
+  '& .MuiSlider-thumb': {
+    height: 27,
+    width: 27,
+    backgroundColor: 'white',
+    border: '1px solid currentColor',
+    '&:hover': {
+      boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
+    },
+    '& .airbnb-bar': {
+      height: 9,
+      width: 1,
+      backgroundColor: 'currentColor',
+      marginLeft: 1,
+      marginRight: 1,
+    },
+  },
+  '& .MuiSlider-track': {
+    height: 3,
+  },
+  '& .MuiSlider-rail': {
+    color: theme.palette.mode === 'dark' ? '#303030' : '#303030',
+    opacity: theme.palette.mode === 'dark' ? undefined : 1,
+    height: 3,
+  },
+}))
+
+function AirbnbThumbComponent(props) {
+  const { children, ...other } = props
+  return (
+    <SliderThumb {...other}>
+      {children}
+      <span className='airbnb-bar' />
+      <span className='airbnb-bar' />
+      <span className='airbnb-bar' />
+    </SliderThumb>
+  )
+}
+
+AirbnbThumbComponent.propTypes = {
+  children: PropTypes.node,
+}
 
 export default function Filters() {
   const localStorageKey = 'filters'
@@ -34,6 +88,28 @@ export default function Filters() {
   const genders = useSelector(state => state.genders)
   const materials = useSelector(state => state.materials)
   const sizes = useSelector(state => state.sizes)
+
+  const [range, setRange] = useState({
+    min: 2000,
+    max: 8000,
+  })
+
+  const maxValue = 10000
+
+  const handleRangeChange = (event, newValue) => {
+    setRange({
+      min: newValue[0],
+      max: newValue[1],
+    })
+  }
+
+  const handleTextFieldChange = event => {
+    const { id, value } = event.target
+    setRange(prevRange => ({
+      ...prevRange,
+      [id]: Number(value),
+    }))
+  }
 
   const [open, setOpen] = useState({
     brands: false,
@@ -107,8 +183,15 @@ export default function Filters() {
     >
       {/* Genders */}
       <ListItemButton onClick={() => handleClick('genders')}>
-        <ListItemText primary='Géneros' />
-        {open.genders ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Géneros'
+          sx={{ color: open.genders ? 'white' : '#42e268' }}
+        />
+        {open.genders ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.genders} timeout='auto' unmountOnExit>
         <FormGroup>
@@ -117,6 +200,7 @@ export default function Filters() {
               key={i}
               control={
                 <Checkbox
+                  sx={{ marginLeft: 2, color: '#42e268' }}
                   checked={
                     checkedFilters['genders']?.includes(gender.id) || false
                   }
@@ -125,6 +209,13 @@ export default function Filters() {
               }
               label={gender.gender}
               id={gender.id}
+              style={{ color: 'white' }}
+              sx={{
+                backgroundColor: '#303030',
+                width: '275px',
+                alignSelf: 'center',
+                marginLeft: 2,
+              }}
             />
           ))}
         </FormGroup>
@@ -132,8 +223,15 @@ export default function Filters() {
 
       {/* Brands */}
       <ListItemButton onClick={() => handleClick('brands')}>
-        <ListItemText primary='Marcas' />
-        {open.brands ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Marcas'
+          sx={{ color: open.brands ? 'white' : '#42e268' }}
+        />
+        {open.brands ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.brands} timeout='auto' unmountOnExit>
         <FormGroup>
@@ -142,6 +240,7 @@ export default function Filters() {
               key={i}
               control={
                 <Checkbox
+                  sx={{ marginLeft: 2, color: '#42e268' }}
                   checked={
                     checkedFilters['brands']?.includes(brand.id) || false
                   }
@@ -150,6 +249,13 @@ export default function Filters() {
               }
               label={brand.brand}
               id={brand.id}
+              style={{ color: 'white' }}
+              sx={{
+                backgroundColor: '#303030',
+                width: '275px',
+                alignSelf: 'center',
+                marginLeft: 2,
+              }}
             />
           ))}
         </FormGroup>
@@ -157,8 +263,15 @@ export default function Filters() {
 
       {/* Categories */}
       <ListItemButton onClick={() => handleClick('categories')}>
-        <ListItemText primary='Categorías' />
-        {open.categories ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Categorías'
+          sx={{ color: open.categories ? 'white' : '#42e268' }}
+        />
+        {open.categories ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.categories} timeout='auto' unmountOnExit>
         <FormGroup>
@@ -167,6 +280,7 @@ export default function Filters() {
               key={i}
               control={
                 <Checkbox
+                  sx={{ marginLeft: 2, color: '#42e268' }}
                   checked={
                     checkedFilters['categories']?.includes(category.id) || false
                   }
@@ -177,6 +291,13 @@ export default function Filters() {
               }
               label={category.category}
               id={category.id}
+              style={{ color: 'white' }}
+              sx={{
+                backgroundColor: '#303030',
+                width: '275px',
+                alignSelf: 'center',
+                marginLeft: 2,
+              }}
             />
           ))}
         </FormGroup>
@@ -184,8 +305,15 @@ export default function Filters() {
 
       {/* Sizes */}
       <ListItemButton onClick={() => handleClick('sizes')}>
-        <ListItemText primary='Tallas' />
-        {open.sizes ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Tallas'
+          sx={{ color: open.sizes ? 'white' : '#42e268' }}
+        />
+        {open.sizes ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.sizes} timeout='auto' unmountOnExit>
         <FormGroup>
@@ -194,20 +322,35 @@ export default function Filters() {
               key={i}
               control={
                 <Checkbox
+                  sx={{ marginLeft: 2, color: '#42e268' }}
                   checked={checkedFilters['sizes']?.includes(size.id) || false}
                   onChange={() => handleCheckboxChange('sizes', size.id)}
                 />
               }
               label={size.size}
               id={size.id}
+              style={{ color: 'white' }}
+              sx={{
+                backgroundColor: '#303030',
+                width: '275px',
+                alignSelf: 'center',
+                marginLeft: 2,
+              }}
             />
           ))}
         </FormGroup>
       </Collapse>
       {/* Colors */}
       <ListItemButton onClick={() => handleClick('colors')}>
-        <ListItemText primary='Colores' />
-        {open.colors ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Colores'
+          sx={{ color: open.colors ? 'white' : '#42e268' }}
+        />
+        {open.colors ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.colors} timeout='auto' unmountOnExit>
         <FormGroup>
@@ -216,6 +359,7 @@ export default function Filters() {
               key={i}
               control={
                 <Checkbox
+                  sx={{ marginLeft: 2, color: '#42e268' }}
                   checked={
                     checkedFilters['colors']?.includes(color.id) || false
                   }
@@ -224,14 +368,28 @@ export default function Filters() {
               }
               label={color.color}
               id={color.id}
+              style={{ color: 'white' }}
+              sx={{
+                backgroundColor: '#303030',
+                width: '275px',
+                alignSelf: 'center',
+                marginLeft: 2,
+              }}
             />
           ))}
         </FormGroup>
       </Collapse>
       {/* Materiales */}
       <ListItemButton onClick={() => handleClick('materials')}>
-        <ListItemText primary='Materiales' />
-        {open.materials ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Materiales'
+          sx={{ color: open.materials ? 'white' : '#42e268' }}
+        />
+        {open.materials ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.materials} timeout='auto' unmountOnExit>
         <FormGroup>
@@ -240,6 +398,7 @@ export default function Filters() {
               key={i}
               control={
                 <Checkbox
+                  sx={{ marginLeft: 2, color: '#42e268' }}
                   checked={
                     checkedFilters['materials']?.includes(material.id) || false
                   }
@@ -250,17 +409,67 @@ export default function Filters() {
               }
               label={material.material}
               id={material.id}
+              style={{ color: 'white' }}
+              sx={{
+                backgroundColor: '#303030',
+                width: '275px',
+                alignSelf: 'center',
+                marginLeft: 2,
+              }}
             />
           ))}
         </FormGroup>
       </Collapse>
       {/* Rango */}
       <ListItemButton onClick={() => handleClick('range')}>
-        <ListItemText primary='Rango de precio' />
-        {open.range ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          primary='Rango de precio'
+          sx={{ color: open.range ? 'white' : '#42e268' }}
+        />
+        {open.range ? (
+          <ExpandLess sx={{ color: 'white' }} />
+        ) : (
+          <ExpandMore sx={{ color: '#42e268' }} />
+        )}
       </ListItemButton>
       <Collapse in={open.range} timeout='auto' unmountOnExit>
-        <List component='div' disablePadding></List>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <TextField
+            id='min'
+            label='Min'
+            value={range.min}
+            onChange={handleTextFieldChange}
+            type='number'
+            InputProps={{
+              style: { color: 'white' },
+            }}
+            sx={{ width: 100, border: '1px solid #42e268', borderRadius: 1 }}
+          />
+          <TextField
+            id='max'
+            label='Max'
+            value={range.max}
+            onChange={handleTextFieldChange}
+            type='number'
+            InputProps={{
+              style: { color: 'white' },
+            }}
+            sx={{ width: 100, border: '1px solid #42e268', borderRadius: 1 }}
+          />
+        </Box>
+        <AirbnbSlider
+          slots={{ thumb: AirbnbThumbComponent }}
+          getAriaLabel={index => (index === 0 ? 0 : 100000)}
+          value={[range.min, range.max]}
+          onChange={handleRangeChange}
+          max={maxValue}
+        />
       </Collapse>
     </List>
   )
