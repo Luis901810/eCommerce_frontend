08cdc28b587a3331ab916b8058ea7e96a803ea70
Alexis Porcelano <alexisporcelano@gmail.com>
2024-01-01 21:14:03 -0300

    direccion del orden establecida

--- src/views/Catalogue/Order.jsx
+++ src/views/Catalogue/Order.jsx
@@ -1,28 +1,80 @@
 import { Box } from '@mui/system'
 import Select from '@mui/material/Select'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import MenuItem from '@mui/material/MenuItem'
 import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
+import SortIcon from '@mui/icons-material/Sort'
+import Button from '@mui/material/Button'
+import VerticalAlignBottomIcon from '@mui/icons-material/VerticalAlignBottom'
+import VerticalAlignTopIcon from '@mui/icons-material/VerticalAlignTop'
+import VerticalAlignCenterIcon from '@mui/icons-material/VerticalAlignCenter'
+import { order } from '../../redux/actions'
+import { useDispatch } from 'react-redux'
 
 export default function Order() {
   const [orderType, setOrderType] = useState('none')
 
+  const [direction, setDirection] = useState({
+    icon: <VerticalAlignCenterIcon sx={{ color: 'white' }} />,
+    type: 'default',
+  })
+
+  const dispatch = useDispatch()
+
   const handleChange = e => {
     setOrderType(e.target.value)
+  }
+
+  const handleButtonClick = () => {
+    switch (direction.type) {
+      case 'default':
+        setDirection({
+          icon: <VerticalAlignTopIcon sx={{ color: 'white' }} />,
+          type: 'asc',
+        })
+        break
+      case 'asc':
+        setDirection({
+          icon: <VerticalAlignBottomIcon sx={{ color: 'white' }} />,
+          type: 'dec',
+        })
+        break
+      case 'dec':
+        setDirection({
+          icon: <VerticalAlignCenterIcon sx={{ color: 'white' }} />,
+          type: 'default',
+        })
+        break
+      default:
+        setDirection({
+          icon: <VerticalAlignCenterIcon sx={{ color: 'white' }} />,
+          type: 'default',
+        })
+        break
+    }
+  }
+
+  const handleOrder = () => {
+    dispatch(order(orderType, direction.type))
   }
 
   return (
     <Box
       sx={{
         height: '40px',
-        width: '200px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
+        justifyContent: 'center'
       }}
     >
-      <FormControl fullWidth>
+      <FormControl
+        fullWidth
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: 3/4
+        }}
+      >
         <InputLabel id='labelInput' sx={{ color: 'white' }}>
           Orden
         </InputLabel>
@@ -38,6 +90,15 @@ export default function Order() {
           <MenuItem value='price'>Precio</MenuItem>
           <MenuItem value='size'>Talla</MenuItem>
         </Select>
+        {orderType !== 'none' ? (
+          <Button variant='outlined' onClick={handleButtonClick}>
+            <SortIcon sx={{ color: 'white' }} />
+            {direction.icon}
+          </Button>
+        ) : null}
+        <Button variant="outlined" onClick={() => handleOrder()} >
+          Aplicar
+        </Button>
       </FormControl>
     </Box>
   )
