d9aa00738667de2f9a76aece45a7e5e9ca27c4b3
Alexis Porcelano <alexisporcelano@gmail.com>
2024-01-02 21:16:18 -0300

    ahora se puede ordenar por tallas

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ import AdbIcon from '@mui/icons-material/Adb'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import Search from './Search'
 import Pages from './Pages'
-
+import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import { useState } from 'react'
 
@@ -146,7 +146,7 @@ const NavBar = () => {
           >
             <Pages />
             <Search />
-            <IconButton aria-label='cart'>
+            <IconButton aria-label='cart' onClick={() => navigate('/ShoppingCart')} >
               <ShoppingCartOutlinedIcon sx={{ color: 'white' }} />
             </IconButton>
           </Box>
