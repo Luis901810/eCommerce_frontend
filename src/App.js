16a05481875ceef3813b57be6565b42bd599a785
EdwardVE <ervasalloe@unal.edu.co>
2024-01-09 02:48:41 -0500

    Flujo de Compra Completa, Perfil de Usuario con su registro de compras, aprovadas, Rechazadas y Pendientes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import UserProfile from './components/UserProfile/UserProfile'
 import FormShoe from './components/Form/FormShoe/FormShoe'
 import Successes from './components/ShoppingCart/Successes/Successes'
 import Failures from './components/ShoppingCart/Failures/Failures'
+import EditProfile from './components/UserProfile/UserOptions/EditProfile/EditProfile'
+import PurchaseHistory from './components/UserProfile/UserOptions/PurchaseHistory/PurchaseHistory'
 
 function App() {
   return (
@@ -23,7 +25,9 @@ function App() {
         <Route path='/Login/Register' element={<Register />}></Route>
         <Route path='/Detail/:idShoe' element={<ShoeDetail />} />
         <Route path='/ShoppingCart' element={<ShoppingCart />}></Route>
-        <Route path='/UserProfile/:idUser' element={<UserProfile />}></Route>
+        <Route path='/UserProfile/:UserEmail' element={<UserProfile />}/>
+        <Route path='/EditProfile/:UserEmail' element={<EditProfile/>}/>
+        <Route path='/PurchaseHistory/:UserEmail' element={<PurchaseHistory />} />
         <Route path='/FormShoe' element={<FormShoe />}></Route>
         <Route path='/Successes'element={<Successes/>}/>
         <Route path='/Failures'element={<Failures/>}/>
